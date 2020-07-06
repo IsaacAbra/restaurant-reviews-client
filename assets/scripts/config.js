010f4a1f9c33b3cb7c0615b95978cfb3d4e1fc48
IsaacAbra <isaac.abramov03@gmail.com>
2020-07-06 16:59:18 -0400

    Here is a fix

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://gentle-anchorage-68298.herokuapp.com/',
+  production: 'https://gentle-anchorage-68298.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
